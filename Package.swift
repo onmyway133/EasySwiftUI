6ec9bd131277e890b9218261efdf95a783ebb020
Khoa Pham <onmyway133@gmail.com>
2020-04-10 23:46:43 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -5,24 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "EasySwiftUI",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "EasySwiftUI",
-            targets: ["EasySwiftUI"]),
+            targets: ["EasySwiftUI"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "EasySwiftUI",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "EasySwiftUITests",
-            dependencies: ["EasySwiftUI"]),
+            dependencies: ["EasySwiftUI"]
+        ),
     ]
 )
