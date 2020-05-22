b919e5bf528e6f54be979ea2ad20ccd6afa779ab
Khoa Pham <onmyway133@gmail.com>
2020-05-22 22:45:19 +0200

    MakeView

---
+++ Sources/EasySwiftUI/View/MakeView.swift
@@ -0,0 +1,21 @@
+//
+//  MakeView.swift
+//  EasySwiftUI
+//
+//  Created by khoa on 22/05/2020.
+//
+
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public struct MakeView: View {
+    let make: () -> AnyView
+
+    public var body: some View {
+        make()
+    }
+}
+
+#endif
