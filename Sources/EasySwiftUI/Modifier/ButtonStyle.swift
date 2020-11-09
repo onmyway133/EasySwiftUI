2315e7263f03fb8e5ad7885eab521afc4e7e2799
Khoa Pham <onmyway133@gmail.com>
2020-11-09 06:52:17 +0100

    Add

--- Sources/EasySwiftUI/Modifier/ButtonStyle.swift
+++ Sources/EasySwiftUI/Modifier/ButtonStyle.swift
@@ -10,17 +10,23 @@ import SwiftUI
 public struct HighlightButtonStyle: ButtonStyle {
     let verticalPadding: CGFloat
     let horizontalPadding: CGFloat
+    let cornerRadius: CGFloat
 
-    public init(verticalPadding: CGFloat = 4, horizontalPadding: CGFloat = 8) {
-        self.verticalPadding = verticalPadding
+    public init(
+        horizontalPadding: CGFloat = 8,
+        verticalPadding: CGFloat = 4,
+        cornerRadius: CGFloat = 4
+    ) {
         self.horizontalPadding = horizontalPadding
+        self.verticalPadding = verticalPadding
+        self.cornerRadius = cornerRadius
     }
 
     public func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
-            .padding(.vertical, verticalPadding)
             .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
             .background(configuration.isPressed ? Color.white.opacity(0.1) : Color.clear)
-            .cornerRadius(2)
+            .cornerRadius(cornerRadius)
     }
 }
