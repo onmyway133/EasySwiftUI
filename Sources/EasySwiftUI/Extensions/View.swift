267767fcab2f6769fa4d994db76c3eb9b358d1ba
Khoa Pham <onmyway133@gmail.com>
2020-12-11 06:18:36 +0100

    Update

--- Sources/EasySwiftUI/Extensions/View.swift
+++ Sources/EasySwiftUI/Extensions/View.swift
@@ -19,6 +19,13 @@ public extension View {
             .lineLimit(nil)
             .fixedSize(horizontal: false, vertical: true)
     }
+
+    func toolTip(_ text: String) -> some View {
+        return self
+            .background(
+                ToolTip(text)
+            )
+    }
 }
 
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
