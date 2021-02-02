80fba084b9f6fd96e7f4a0552384b5079687ae9a
Khoa Pham <onmyway133@gmail.com>
2021-02-02 13:04:56 +0100

    Update

--- Sources/EasySwiftUI/View/MakeView.swift
+++ Sources/EasySwiftUI/View/MakeView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct MakeView<Content: View>: View {
-    let make: () -> Content
+    let content: Content
 
-    public init(make: @escaping () -> Content) {
-        self.make = make
+    public init(@ViewBuilder make: () -> Content) {
+        self.content = make()
     }
 
     public var body: some View {
-        make()
+        content
     }
 }
