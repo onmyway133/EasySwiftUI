9ea7a762db14cc767606edf3bcb869082328b8d6
Khoa Pham <onmyway133@gmail.com>
2021-03-18 06:28:07 +0100

    Add views

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EasySwiftUI",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v11),
         .iOS(.v13)
     ],
     products: [
